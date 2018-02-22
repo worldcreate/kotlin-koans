2d78a2727cc7ba6dadf9968530cb51f7608bd3d7
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2018-02-22 23:34:04 +0900

    fix/19

--- src/ii_collections/n19Sum.kt
+++ src/ii_collections/n19Sum.kt
@@ -8,4 +8,4 @@ fun example6() {
 fun Customer.getTotalOrderPrice(): Double =
     // Return the sum of prices of all products that a customer has ordered.
     // Note: a customer may order the same product for several times.
-    this.orders.sumByDouble {it.products.sumByDouble{it.price}}
+    this.orders.flatMap{it.products}.sumByDouble{it.price}
