a6a5dbd76eaa279cc998c9871c9daf77464e3ff0
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2018-03-04 01:01:08 +0900

    fix ii.23

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,7 +2,7 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> =
     // Return the set of customers who ordered the specified product
-    this.customers.filter {it.orders.flatMap {it.products}.any {it == product}}.toSet()
+    this.customers.filter {it.orderedProducts.contains(product)}.toSet()
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? =
     // Return the most expensive product among all delivered products
@@ -12,4 +12,4 @@ fun Customer.getMostExpensiveDeliveredProduct(): Product? =
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int =
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    this.customers.flatMap {it.orders}.flatMap{it.products}.filter {it == product}.count()
+    this.customers.flatMap {it.orders}.flatMap{it.products}.count {it == product}
